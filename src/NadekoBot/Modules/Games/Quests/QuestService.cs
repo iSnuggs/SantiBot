ba89b387679efaf3adf183a0d4aeaa0c87627224
Breaker <nadekodiscordbot@gmail.com>
2025-03-28 03:17:56 +0100

    ﻿fix: color typereader fix fix: delmsgoncmd fix: guildconfig init

--- src/NadekoBot/Modules/Games/Quests/QuestService.cs
+++ src/NadekoBot/Modules/Games/Quests/QuestService.cs
@@ -46,34 +46,44 @@ public sealed class QuestService(
 
         _ = Task.Run(async () =>
         {
-            // Log.Information("Action reported by {UserId}: {EventType} {Metadata}",
-                // userId,
-                // eventType,
-                // metadata.ToJson());
+            Log.Information("Action reported by {UserId}: {EventType} {Metadata}",
+                userId,
+                eventType,
+                metadata.ToJson());
             metadata ??= new();
             var now = DateTime.UtcNow;
 
+            Log.Information("done?");
             var alreadyDone = await botCache.GetAsync(UserCompletedDailiesKey(userId));
             if (alreadyDone.IsT0)
                 return;
+            
+            Log.Information("not done");
 
             var userQuests = await GetUserQuestsAsync(userId, now);
 
+            Log.Information("got quests");
             foreach (var (q, uq) in userQuests)
             {
                 // deleted quest
                 if (q is null)
                     continue;
+                
+                Log.Information("not deleted {QuestEventType} - {EventType}", q.EventType, eventType);
 
                 // user already completed or incorrect event
                 if (uq.IsCompleted || q.EventType != eventType)
                     continue;
+                
+                Log.Information("Gonna update progress");
 
                 var newProgress = q.TryUpdateProgress(metadata, uq.Progress);
 
                 // user already did that part of the quest
                 if (newProgress == uq.Progress)
                     continue;
+                
+                Log.Information("new progress");
 
                 var isCompleted = newProgress >= q.RequiredAmount;
 
--- src/NadekoBot/_common/TypeReaders/Rgba32TypeReader.cs
+++ src/NadekoBot/_common/TypeReaders/Rgba32TypeReader.cs
@@ -8,7 +8,29 @@ public sealed class Rgba32TypeReader : NadekoTypeReader<Rgba32>
 {
     public override ValueTask<TypeReaderResult<Rgba32>> ReadAsync(ICommandContext context, string input)
     {
+        if (!Color.TryParse(input, out var color))
+        {
+            Log.Information("Fail");
+            return ValueTask.FromResult(
+                TypeReaderResult.FromError<Rgba32>(CommandError.ParseFailed, "Parameter is not a valid color hex."));
+        }
+        Log.Information(color.ToHex());
+
+        return ValueTask.FromResult(TypeReaderResult.FromSuccess((Rgba32)color));
+
         if (Rgba32.TryParseHex(input, out var clr))
+        {
+            return ValueTask.FromResult(TypeReaderResult.FromSuccess(clr));
+        }
+
+        if (!Enum.TryParse<Color>(input, true, out var clrName))
+            return ValueTask.FromResult(
+                TypeReaderResult.FromError<Rgba32>(CommandError.ParseFailed,
+                    "Parameter is not a valid color hex."));
+
+        Log.Information(clrName.ToString());
+
+        if (Rgba32.TryParseHex(clrName.ToHex(), out clr))
         {
             return ValueTask.FromResult(TypeReaderResult.FromSuccess(clr));
         }
