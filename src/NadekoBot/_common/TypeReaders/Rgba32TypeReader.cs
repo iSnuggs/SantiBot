250962dbc3b77dbb431e9181ab5c225f81bdb8b3
Breaker <nadekodiscordbot@gmail.com>
2025-03-28 19:35:35 +0100

    ﻿dev: simplified quota system

--- src/NadekoBot/_common/TypeReaders/Rgba32TypeReader.cs
+++ src/NadekoBot/_common/TypeReaders/Rgba32TypeReader.cs
@@ -14,28 +14,7 @@ public sealed class Rgba32TypeReader : NadekoTypeReader<Rgba32>
             return ValueTask.FromResult(
                 TypeReaderResult.FromError<Rgba32>(CommandError.ParseFailed, "Parameter is not a valid color hex."));
         }
-        Log.Information(color.ToHex());
 
         return ValueTask.FromResult(TypeReaderResult.FromSuccess((Rgba32)color));
-
-        if (Rgba32.TryParseHex(input, out var clr))
-        {
-            return ValueTask.FromResult(TypeReaderResult.FromSuccess(clr));
-        }
-
-        if (!Enum.TryParse<Color>(input, true, out var clrName))
-            return ValueTask.FromResult(
-                TypeReaderResult.FromError<Rgba32>(CommandError.ParseFailed,
-                    "Parameter is not a valid color hex."));
-
-        Log.Information(clrName.ToString());
-
-        if (Rgba32.TryParseHex(clrName.ToHex(), out clr))
-        {
-            return ValueTask.FromResult(TypeReaderResult.FromSuccess(clr));
-        }
-
-        return ValueTask.FromResult(
-            TypeReaderResult.FromError<Rgba32>(CommandError.ParseFailed, "Parameter is not a valid color hex."));
     }
 }
