ae69ce0282f138f8e6edf195bf2d31cb2a6cb82e
Breaker <nadekodiscordbot@gmail.com>
2026-03-13 07:03:25 +0000

    fix: fix docker data/lib handling

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,9 @@ data="/app/data"
 cp -R -n $data_init/* $data
 cp -n "$data_init/creds_example.yml" "$data/creds.yml"
 
+# Always overwrite native libraries to ensure updates are applied
+cp -f "$data_init/lib/"*.so "$data/lib/" 2>/dev/null || true
+
 yt-dlp -U || true
 
 exec "$@"
